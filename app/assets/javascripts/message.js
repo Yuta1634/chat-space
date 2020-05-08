1d62546ed8670d8af663775e234dfde3acd9ce06
Yuta1634 <yuta1994year@gmail.com>
2020-05-08 11:54:06 +0900

    create Automatic-updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,9 @@
 $(function(){
-
+  
   function buildHTML(message){
     if (message.image){
       var html =
-        `<div class='main-chat__messages'>
+        `<div class='main-chat__messages' data-message-id=${message.id}>
           <div class='main-chat__message'>
             <p class='main-chat__messaghe-member'>
               ${message.user_name}
@@ -22,7 +22,7 @@ $(function(){
         return html;
     } else {
       var html =
-      `<div class='main-chat__messages'>
+      `<div class='main-chat__messages' data-message-id=${message.id}>
         <div class='main-chat__message'>
           <p class='main-chat__messaghe-member'>
             ${message.user_name}
@@ -64,4 +64,30 @@ $(function(){
         alert("メッセージの送信に失敗しました。")
       });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.main-chat__messages').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
