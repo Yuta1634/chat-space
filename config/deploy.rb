25e08de7efc563a1d840fd03d8ee70446154a857
Yuta1634 <yuta1994year@gmail.com>
2020-05-12 11:01:47 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,11 +12,11 @@ set :repo_url,  'git@github.com:Yuta1634/Yuta.1634.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '(2.5.1)' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['(~/.ssh/ClariS1634.pem)'] 
+                  keys: ['~/.ssh/ClariS1634.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
