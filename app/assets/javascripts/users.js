37e7b4837e2794c88087ac6a86625a66e792889c
Yuta1634 <yuta1994year@gmail.com>
2020-05-15 22:48:04 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,6 @@
 $(function() {
   function addUser(user) {
-    let html = `
+    var html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
         <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
@@ -10,7 +10,7 @@ $(function() {
   }
 
   function addNoUser() {
-    let html = `
+    var html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">ユーザーが見つかりません</p>
       </div>
@@ -18,7 +18,7 @@ $(function() {
     $("#user-search-result").append(html);
   }
   function addDeleteUser(name, id) {
-    let html = `
+    var html = `
     <div class="chat-group-user clearfix" id="${id}">
       <p class="chat-group-user__name">${name}</p>
       <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
@@ -26,11 +26,11 @@ $(function() {
     $(".js-add-user").append(html);
   }
   function addMember(userId) {
-    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    var html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
   }
   $("#user-search-field").on("keyup", function() {
-    let input = $("#user-search-field").val();
+    var input = $("#user-search-field").val();
     $.ajax({
       type: "GET",
       url: "/users",
